67c373e8a4cdee86dda7f5ba10eb55e20b49b104
Yeyoon <875776209@qq.com>
2020-01-01 23:37:50 +0800

    add dlambda implementations

--- lol.lisp
+++ lol.lisp
@@ -1,3 +1,18 @@
 ;;;; lol.lisp
 
 (in-package #:lol)
+
+;;; This is the dlambda implemented
+;;; I just do not know whether it is
+;;; has free variable capture issue.
+(defmacro dlambda (&rest ds)
+  (let ((as (gensym "as")))
+    `(lambda (&rest ,as)
+       (case (car ,as)
+	 ,@(loop for d in ds collect
+		 `(,(car d)
+		   (apply (lambda ,(second d) ,(third d))
+			  ,(if (eq t (car d))
+			      `,as
+			      `(cdr ,as)))))))))
+
