346de399e1614d459e2d82edf162ba90a8d20dfd
Yeyoon <875776209@qq.com>
2020-01-05 00:29:29 +0800

    add the macro defunits [1]. Cycle chain not detected and not support this style: (defunits time s   m 60) must write (defunits time s m (60 s)). Both will be corrected in the next push.

--- lol.lisp
+++ lol.lisp
@@ -22,3 +22,94 @@
 ;;; flatten is not work in sbcl
 ;;; for ,x is not '(, x) is a
 ;;; struct not a list?
+(defun flatten (tree)
+  (labels ((rec (tree result)
+	     (cond ((consp tree)
+		    (append (rec (car tree) result)
+			    (rec (cdr tree) result)))
+		   #+sbcl
+		   ((sb-impl::comma-p tree)
+		    (list (sb-impl::comma-expr tree)))
+		   (t  (list tree)))))
+    (remove '() (rec tree '()))))
+
+
+;;; chapter 3
+;;;  sleep-uints%
+;;;
+(defun sleep-units% (value unit)
+  (sleep
+   (* value
+      (case unit
+	((s) 1)
+	((m) 60)
+	((h) 3600)
+	((d) 86400)
+	((ms) 1/1000)
+	((us) 1/1000000)))))
+
+
+(defmacro unit-of-time (value unit)
+  `(* ,value
+      ,(case unit
+	((s) 1)
+	((m) 60)
+	((h) 3600)
+	((d) 86400)
+	((ms) 1/1000)
+	((us) 1/1000000))))
+
+(defun symb (&rest args)
+  (values (intern 
+	   (with-output-to-string (s)
+	     (dolist (x args)
+	       (princ x s))))))
+
+
+(defun group (lists n)
+  (if (zerop n) (error "n is zero, invalid."))
+  (if (> (length lists) n)
+      (cons (subseq lists 0 n)
+	    (group (nthcdr n lists) n))
+      (list lists)))
+
+;;; chapter 5
+;;; defuints
+;;;
+(defmacro defunits (quantity base-unit &rest units)
+  (let ((value (gensym "value"))
+	(unit (gensym "unit")))
+    `(defmacro ,(symb 'unit-of- quantity) (,value ,unit)
+       `(* ,,value
+	   ,(case ,unit
+	      ((,`,base-unit) 1)
+	      ,@(expand-units `,base-unit (group `,units 2))))))) 
+
+
+#|
+(defunits time 
+  s
+  m (1/60 h)
+  h (60 m))
+|#
+
+(defun get-unit-chain (u units)
+  (car (member u units :test #'eq :key #'car)))
+
+(defun chain-unit (u base-unit units)
+  (if (eq base-unit u)
+      1
+      (* (caadr (get-unit-chain u units))
+	 (chain-unit (cadadr (get-unit-chain u units)) base-unit units))))
+
+(defun expand-unit (unit base-unit &rest units)
+  (let ((lu (group units 2)))
+    (chain-unit unit base-unit lu)))
+
+
+(defun expand-units (base-unit units)
+  (loop :for u :in units :collect
+	`((,(car u)) ,(chain-unit (car u) base-unit units))))
+       
+  
+  
