258127de2463a27f5964bdd31c764afc7e5388ca
zhenghe <zhenghe@cisco.com>
2020-01-15 19:30:44 +0800

    add forth

--- lol.lisp
+++ lol.lisp
@@ -930,3 +930,152 @@ ichain-before
 
        
        
+
+;; chapter 8
+(defvar forth-registers
+  '(pstack rstack pc dict compiling dtable))
+
+(defstruct forth-word
+  name prev immediate thread)
+
+(defun forth-lookup (w last)
+  (if last
+      (if (eql (forth-word-name last) w)
+	  last
+	  (forth-lookup
+	   w (forth-word-prev last)))))
+
+(defmacro forth-inner-interpreter ()
+  '(loop
+      do (cond
+	   ((functionp (car pc))
+	    (funcall (car pc)))
+	   ((consp (car pc))
+	    (push (cdr pc) rstack)
+	    (setf pc (car pc)))
+	   ((null pc)
+	    (setf pc (pop rstack)))
+	   (t
+	    (push (car pc) pstack)
+	    (setf pc (cdr pc))))
+      until (and (null pc) (null rstack))))
+
+
+(defvar forth-prim-forms nil)
+
+(defmacro def-forth-naked-prim (&rest code)
+  `(push ',code forth-prim-forms))
+
+(defmacro def-forth-prim (&rest code)
+  `(def-forth-naked-prim
+       ,@code
+       (setf pc (cdr pc))))
+
+;; BASIC-PRIM-FORMS
+(def-forth-prim nop nil)
+
+(def-forth-prim * nil
+  (push (* (pop pstack) (pop pstack))
+	pstack))
+
+(def-forth-prim drop nil
+  (pop pstack))
+
+(def-forth-prim dup nil
+  (push (car pstack) pstack))
+
+(def-forth-prim swap nil
+  (rotatef (car pstack) (cadr pstack)))
+
+(def-forth-prim print nil
+  (print (pop pstack)))
+
+(def-forth-prim >r nil
+  (push (pop pstack) rstack))
+
+(def-forth-prim r> nil
+  (push (pop rstack) pstack))
+
+
+;; template test new forth
+(defmacro new-forth ()
+  `(alet ,forth-registers
+     (setq dtable (make-hash-table))
+     (forth-install-prims)
+     (dolist (v forth-stdlib)
+       (funcall this v))
+     (plambda (v) ,forth-registers
+       (let ((word (forth-lookup v dict)))
+	 (if word
+	     (forth-handle-found)
+	     (forth-handle-not-found))))))
+
+
+(defmacro forth-install-prims ()
+  `(progn
+     ,@(mapcar
+	#`(let ((thread (lambda () ,@(cddr a1))))
+	    (setf dict
+		  (make-forth-word
+		   :name ',(car a1)
+		   :prev dict
+		   :immediate ,(cadr a1)
+		   :thread thread))
+	    (setf (gethash thread dtable)
+		  ',(cddr a1)))
+	forth-prim-forms)))
+
+(def-forth-prim [ t
+  (setf compiling nil))
+
+(def-forth-prim ] nil
+  (setf compiling t))
+
+(defmacro forth-compile-in (v)
+  `(setf (forth-word-thread dict)
+	 (nconc (forth-word-thread dict)
+		(list ,v))))
+
+(defmacro forth-handle-found ()
+  `(if (and compiling
+	    (not (forth-word-immediate word)))
+       (forth-compile-in (forth-word-thread word))
+       (progn
+	 (setf pc (list (forth-word-thread word)))
+	 (forth-inner-interpreter))))
+
+
+(defmacro forth-handle-not-found ()
+    `(cond
+       ((and (consp v) (eq (car v) 'quote))
+	(if compiling
+	    (forth-compile-in (cadr v))
+	    (push (cadr v) pstack)))
+       ((and (consp v) (eq (car v) 'postpone))
+	(let ((word (forth-lookup (cadr v) dict)))
+	  (if (not word)
+	      (error "Postpone failed : ~a" (cadr v)))
+	  (forth-compile-in (forth-word-thread word))))
+       ((symbolp v)
+	(error "Word ~a not found" v))
+       (t
+	(if compiling
+	    (forth-compile-in v)
+	    (push v pstack)))))
+
+(defmacro! go-forth (o!forth &rest words)
+  `(dolist (w ',words)
+     (funcall ,g!forth w)))
+
+(defvar forth-stdlib nil)
+
+(defmacro forth-stdlib-add (&rest all)
+  `(setf forth-stdlib
+	 (nconc forth-stdlib
+		',all)))
+
+	      
+
+
+
+
