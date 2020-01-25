08d27e252973b2986b9d92d58bdf0a1c06b580af
zhenghe <zhenghe@cisco.com>
2020-01-25 14:34:49 +0800

    add ch8

---
+++ ch8.lisp
@@ -0,0 +1,67 @@
+(in-package #:lol)
+
+(defvar forth-registers
+  '(pstack rstack pc dict compiling dtable))
+
+(defstruct forth-word
+  name prev immediate thread)
+
+(defun forth-lookup (w last)
+  (if last
+      (if (eql (forth-word-name last) w)
+	  last
+	  (forth-lookup
+	   w (forth-word-prev last)))))
+
+(defmacro forth-inner-interpreter ()
+  '(loop
+      do (cond
+	   ((functionp (car pc))
+	    (funcall (car pc)))
+	   ((consp (car pc))
+	    (push (cdr pc) rstack)
+	    (setf pc (car pc)))
+	   ((null pc)
+	    (setf pc (pop rstack)))
+	   (t
+	    (push (car pc) pstack)
+	    (setf pc (cdr pc))))
+      until (and (null pc) (null rstack))))
+
+;; Prim-form: (name immediate . forms)
+(defvar forth-prim-forms nil)
+
+(defmacro def-forth-naked-prim (&rest code)
+  `(push ',code forth-prim-forms))
+
+(defmacro def-forth-prim (&rest code)
+  '(def-forth-naked-prim
+    ,@code
+    (setf pc (cdr pc))))
+
+
+;; 8.6 BASIC-PRIM-FORMS
+
+(def-forth-prim nop nil)
+
+(def-forth-prim * nil
+  (push (* (pop pstack) (pop pstack))
+	pstack))
+
+(def-forth-prim drop nil
+  (pop pstack))
+
+(def-forth-prim dup nil
+  (push (car pstack) pstack))
+
+(def-forth-prim swap nil
+  (rotatef (car pstack) (cadr pstack)))
+
+(def-forth-prim print nil
+  (print (pop pstack)))
+
+(def-forth-prim >r nil
+  (push (pop pstack) rstack))
+
+(def-forth-prim r> nil
+  (push (pop rstack) pstack))
